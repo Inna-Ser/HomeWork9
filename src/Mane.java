9252e74192c587179332c1faeb53d485b8525724
Inna <serebryakovainna@mail.ru>
2022-11-15 01:10:05 +0200

    initial commit

---
+++ src/Mane.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+import static java.lang.System.*;
+
+public class Mane {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+// Задача № 1 Сумма
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
+
